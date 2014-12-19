7669ba937b50218ca55948c69a38b2be326fb50c
federicomaffei <federico.maffei@gmail.com>
2014-12-19 00:31:45 +0000

    static files now served, caching disabled for dev environment

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-var Hapi = require('hapi');
+var Hapi = require('hapi'),
+Path     = require('path'),
+nodemon  = require('nodemon');
 
 var server = new Hapi.Server();
 server.connection({ port: 3000 });
 server.views({
-    engines: { jade: require('jade') },
+    engines: { 
+    	jade: { 
+    		module: require('jade'),
+    		isCached: false
+    	}
+    },
     path: __dirname + '/views'
+});
+
+server.route({
+    method: "GET",
+    path: "/assets/{path*}",
+    handler: {
+        directory: {
+            path: "./assets",
+            listing: false,
+            index: false
+        }
+    }
 });
 
 server.route({
